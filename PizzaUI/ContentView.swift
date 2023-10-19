733b001ffb0269bde8a8f29547ecad5a22388caa
brsaca <brsaca@gmail.com>
2023-10-19 10:34:42 -0600

    Initial Commit

---
+++ PizzaUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PizzaUI
+//
+//  Created by Brenda Saavedra Cantu on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PizzaUI/PizzaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaUIApp.swift
+//  PizzaUI
+//
+//  Created by Brenda Saavedra Cantu on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PizzaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
