c08c501d16cf21fa493cbb715a8b33f38e04bb0e
brsaca <brsaca@gmail.com>
2023-10-19 12:52:13 -0600

    style: add colors

---
+++ PizzaUI/Extension/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  PizzaUI
+//
+//  Created by Brenda Saavedra Cantu on 19/10/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let pBackground = Color("background")
+    static let pButton = Color("primaryButton")
+}
